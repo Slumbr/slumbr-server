73fc92ed679dd71c598d7724282dc08e561b9564
Robbie Coomber <robbie@robbiecoomber.com>
2018-10-31 12:45:37 +0000

    Fix the logging to not duplicate log lines

--- src/logging.ts
+++ src/logging.ts
@@ -3,6 +3,23 @@ import { config } from "./config";
 import * as winston from "winston";
 
 export function logger(winstonInstance: typeof winston) {
+  winstonInstance.configure({
+    level: config.debugLogging ? "debug" : "info",
+    transports: [
+      //
+      // - Write all logs error (and below) to `error.log`.
+      new winston.transports.File({ filename: "error.log", level: "error" }),
+      //
+      // - Write to all logs with specified level to console.
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.simple()
+        )
+      })
+    ]
+  });
+
   return async (ctx: Koa.Context, next: () => Promise<any>) => {
     const start = new Date().getMilliseconds();
 
@@ -20,23 +37,6 @@ export function logger(winstonInstance: typeof winston) {
     const msg: string = `${ctx.method} ${ctx.originalUrl} ${
       ctx.status
     } ${ms}ms`;
-
-    winstonInstance.configure({
-      level: config.debugLogging ? "debug" : "info",
-      transports: [
-        //
-        // - Write all logs error (and below) to `error.log`.
-        new winston.transports.File({ filename: "error.log", level: "error" }),
-        //
-        // - Write to all logs with specified level to console.
-        new winston.transports.Console({
-          format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.simple()
-          )
-        })
-      ]
-    });
 
     winstonInstance.log(logLevel, msg);
   };
