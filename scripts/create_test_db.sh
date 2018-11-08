efd2a7dff2e89657fdb9c4684339bc991be34beb
Robbie Coomber <robbie@robbiecoomber.com>
2018-11-08 12:18:06 +0000

    Make test db setup script drop user/db if exists
    So that it's possible to re-run the script on a machine

--- scripts/create_test_db.sh
+++ scripts/create_test_db.sh
@@ -4,6 +4,8 @@ set -e
 
 # Must be run as someone with DB admin powers, e.g. the postgres user
 
+psql -c "DROP DATABASE IF EXISTS \"slumbr-test\";" -U postgres
+psql -c "DROP USER IF EXISTS \"slumbr-test\";" -U postgres
 psql -c "CREATE USER \"slumbr-test\" WITH PASSWORD 'slumbr-test';" -U postgres
 psql -c "CREATE DATABASE \"slumbr-test\" WITH OWNER 'slumbr-test';" -U postgres
 psql -c 'CREATE EXTENSION "uuid-ossp";' -U postgres -d slumbr-test
